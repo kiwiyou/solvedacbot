fe3bd793631516b0b234382dfd7cb59962e2a6c8
kiwiyou <kiwiyou.dev@gmail.com>
2021-11-04 15:09:06 +0900

    Add problem and user command

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
+use std::result::Result;
+use telbot_cf_worker::types::message::SendMessage;
 use telbot_cf_worker::types::query::AnswerInlineQuery;
 use telbot_cf_worker::types::update::*;
 use worker::*;
 
+use crate::command::Command;
+
+mod command;
 mod formatter;
 mod solved;
 mod utils;
@@ -17,7 +22,7 @@ fn log_request(req: &Request) {
 }
 
 #[event(fetch)]
-pub async fn main(req: Request, env: Env) -> Result<Response> {
+pub async fn main(req: Request, env: Env) -> worker::Result<Response> {
     log_request(&req);
     utils::set_panic_hook();
     let token = env.secret("BOT_TOKEN")?.to_string();
@@ -27,7 +32,6 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
     router
         .post_async(&bot_endpoint, |mut req, ctx| async move {
             let update = req.json::<Update>().await.unwrap();
-            #[allow(clippy::single_match)]
             match update.kind {
                 UpdateKind::InlineQuery { inline_query } => {
                     let (page, parity) = match inline_query.offset.parse::<u32>() {
@@ -52,6 +56,64 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
                             answer_query.with_next_offset((page * 2 + parity).to_string());
                     }
                     ctx.data().send_json(&answer_query).await.unwrap();
+                }
+                UpdateKind::Message { message } => {
+                    if let Some(text) = message.text() {
+                        let command = Command::new(text);
+                        let mut args = command.args();
+                        match command.label {
+                            "/problem" => {
+                                match args.map(str::parse).collect::<Result<Vec<_>, _>>() {
+                                    Ok(id_list) if !id_list.is_empty() => {
+                                        let problems = solved::problem_lookup(&id_list).await?;
+                                        if problems.is_empty() {
+                                            let req = SendMessage::new(
+                                                message.chat.id,
+                                                "문제를 찾을 수 없습니다.",
+                                            );
+                                            ctx.data().send_json(&req).await.unwrap();
+                                        } else {
+                                            let req = formatter::problem_show_to_message(
+                                                message.chat.id,
+                                                &problems,
+                                            );
+                                            ctx.data().send_json(&req).await.unwrap();
+                                        }
+                                    }
+                                    _ => {
+                                        let req = SendMessage::new(
+                                            message.chat.id,
+                                            "사용법: /problem <문제번호1> <문제번호2> <...>",
+                                        );
+                                        ctx.data().send_json(&req).await.unwrap();
+                                    }
+                                }
+                            }
+                            "/user" => {
+                                if let Some(handle) = args.next() {
+                                    let user = solved::user_show(handle).await?;
+                                    if let Some(user) = user {
+                                        let req =
+                                            formatter::user_show_to_message(message.chat.id, user);
+                                        ctx.data().send_file(&req).await.unwrap();
+                                    } else {
+                                        let req = SendMessage::new(
+                                            message.chat.id,
+                                            "사용자를 찾을 수 없습니다.",
+                                        );
+                                        ctx.data().send_json(&req).await.unwrap();
+                                    }
+                                } else {
+                                    let help = SendMessage::new(
+                                        message.chat.id,
+                                        "사용법: /user <사용자명>",
+                                    );
+                                    ctx.data().send_json(&help).await.unwrap();
+                                }
+                            }
+                            _ => {}
+                        }
+                    }
                 }
                 _ => {}
             }
